1696d0e4c8111ff17628728cb88daf65a78e6744
AbhijeetAS <astars13112000@gmail.com>
2023-01-03 17:30:20 +0530

    initial commit

---
+++ hello.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class hello {
+    public static void main(String[] args) {
+        
+       String str=new String("Harry");
+       System.out.println(str);
+        
+    }
+    
+}
